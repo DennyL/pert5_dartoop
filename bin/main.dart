3493f6afc338d359d607cd5005d6995abe22f670
Denny Lianto <c14190004@john.petra.ac.id>
2022-03-14 10:22:20 +0700

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -17,22 +17,33 @@ void main() {
       "Jumlah Sks Punya ${mhs2.nama} adalah ${mhs2.Sks} dengan Status ${mhs2.status}");
   // print(mhs2.lihatipk(mhs2));
 
+  print("--------------------------------------- \n");
+
   //Dosen LB
   var dsnlb1 = Dosen('Yayan', '141842', 0, 0);
   dsnlb1.dnslbjumsks(dsnlb1, 42);
-  print(dsnlb1.dsnlblihatpenghasilan(dsnlb1));
+  print(
+      "Dosen LB ${dsnlb1.nama} mengampu SKS sejumlah ${dsnlb1.jumsks} dan memiliki penghasilan sebesar ${dsnlb1.dsnlblihatpenghasilan(dsnlb1)}");
+
+  print("--------------------------------------- \n");
 
   //Dosen Tamu
   var dsntamu1 = DosenTamu('Bruno', '181902', 0, 0, 2500);
   dsntamu1.dsntamuabsensi(dsntamu1);
   dsntamu1.dnstamujumsks(dsntamu1, 43);
-  print(dsntamu1.dsntamulihatpenghasilan(dsntamu1));
+  print(
+      "Dosen LB ${dsntamu1.nama} mengampu SKS sejumlah ${dsntamu1.jumsks} dan memiliki penghasilan sebesar ${dsntamu1.dsntamulihatpenghasilan(dsntamu1)}");
+
+  print("--------------------------------------- \n");
 
   //Dosen Tetap
   var dsnttp1 = DosenTetap('Agus', '160123', 0, 0, 5000, 3500000);
   dsnttp1.dsnttpabsensi(dsnttp1);
   dsnttp1.dnsttpjumsks(dsnttp1, 43);
-  print(dsnttp1.dsnttplihatpenghasilan(dsnttp1));
+  print(
+      "Dosen LB ${dsnttp1.nama} mengampu SKS sejumlah ${dsnttp1.jumsks} dan memiliki penghasilan sebesar ${dsnttp1.dsnttplihatpenghasilan(dsnttp1)}");
+
+  print("--------------------------------------- \n");
 
   //Staff
   var staf1 = Staf('Udin', '13141', 0, 12, 2500, 1500000);
@@ -40,4 +51,6 @@ void main() {
   staf1.staffambilcuti(staf1);
   print(
       "Staf ${staf1.nama} memiliki penghasilan sebesar ${staf1.stafflihatpenghasilan(staf1)} dan batas Cuti tersisa ${staf1.batascuti} dan Kehadiran sebanyak ${staf1.absensi}");
+
+  print("--------------------------------------- \n");
 }
